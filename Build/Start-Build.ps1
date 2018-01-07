6524b25ba77dde7f846514b796e0803515fbc203
Christian Baumgartner <github@treegardner.de>
2018-01-07 22:12:57 +0100

    !deploy

--- Build/Start-Build.ps1
+++ Build/Start-Build.ps1
@@ -1,17 +1,23 @@
-﻿param(
+﻿Param(
+
     $Task = 'Default'
+
 )
 
-# dependencies
+#Dependencies
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-if(-not (Get-Module -ListAvailable PSDepend))
+
+If (-not (Get-Module -ListAvailable PSDepend))
 {
     & (Resolve-Path "$PSScriptRoot\helpers\Install-PSDepend.ps1")
 }
+
 Import-Module PSDepend
-$null = Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force
+
+$Null = Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force
 
 Set-BuildEnvironment
 
 Invoke-psake $PSScriptRoot\psake.ps1 -taskList $Task -nologo
-exit ( [int]( -not $psake.build_success ) )
+
+Exit ( [int]( -not $psake.build_success ) )
